6d239d6a365fc30fc579260e160ccbb9218ce681
yrral <yrralson1@gmail.com>
2023-04-12 10:38:25 +0300

    Inclure les urls account/login, account/logout, account/... | configuration de la page de connexion

--- appointment/appointment/settings.py
+++ appointment/appointment/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'appointment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,3 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/"
