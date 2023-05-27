3a95c665107064ad3f0e3c95208fcd255bf89118
yrral <yrralson1@gmail.com>
2023-05-27 17:56:19 +0300

    Page d'inscription: Username, Password, Password confirmation: http://localhost:8000/register/

---
+++ appointment/register/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
