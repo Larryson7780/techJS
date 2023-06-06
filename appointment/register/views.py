0ae9d6585e5e2170c7298f4ef419c8bac2d8a5d2
yrral <yrralson1@gmail.com>
2023-06-06 15:51:13 +0300

    message ENREGISTREMENT AVEC SUCCES / ERREUR DE L'ENREGISTREMENT

--- appointment/register/views.py
+++ appointment/register/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
     form = UserCreationForm()
@@ -7,5 +8,8 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.info(request, "Inscription réussie")
+        else:
+            messages.info(request, "votre mot de passe doit contenir au moins 8 caractères / votre mot de passe ne peut pas être un mot de passe couramment utilisé / votre mot de passe ne peut pas être entièrement numérique")
     context = {'form': form}
     return render(request, 'registration/register.html', context)
